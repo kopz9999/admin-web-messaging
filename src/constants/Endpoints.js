aa6b2d1aa6e7f06e379628b5db22bc90afc06b92
Kyoto Kopz <kopz9999@gmail.com>
2016-05-31 02:15:45 -0700

    timeline_base # updated API protocol

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,13 +1,13 @@
 export const EVENTS_API =
-  'http://private-9b421-events59.apiary-mock.com/events';
+  'https://private-9b421-events59.apiary-mock.com/events';
 export const EVENTS_QUERY_API =
-  'http://private-9b421-events59.apiary-mock.com/events/query';
+  'https://private-9b421-events59.apiary-mock.com/events/query';
 export const EVENTS_QUERY_COUNT_API =
-  'http://private-9b421-events59.apiary-mock.com/events/query/count';
+  'https://private-9b421-events59.apiary-mock.com/events/query/count';
 
 export const PAGES_API =
-  'http://private-9b421-events59.apiary-mock.com/pages';
+  'https://private-9b421-events59.apiary-mock.com/pages';
 export const USERS_API =
-  'http://private-9b421-events59.apiary-mock.com/users';
+  'https://private-9b421-events59.apiary-mock.com/users';
 export const SITES_API =
-  'http://private-9b421-events59.apiary-mock.com/sites';
+  'https://private-9b421-events59.apiary-mock.com/sites';
