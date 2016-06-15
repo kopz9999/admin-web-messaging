5966dc4c6ed97bd183da11eb6a427230c6f7eaf8
Kyoto Kopz <kopz9999@gmail.com>
2016-06-15 00:59:07 -0700

    backend # added participants backend handler

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Resource = require('express-resource');
+var ParticipantsController = require('./resources/participants');
 
 var app = express();
+var router = express.Router();
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -14,6 +17,12 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-app.resource('api/events', require('./events'), { format: 'json' });
+app.resource('api/events', require('./resources/events'), { format: 'json' });
+
+var participantsController = new ParticipantsController(
+  router.route('/conversations/:conversationId/participants')
+);
+
+app.use('/api', router);
 
 module.exports = app;
---
+++ server/resources/participants.js
@@ -0,0 +1,58 @@
+var LayerAPI = require('layer-api');
+var API_TOKEN = 'PQ4e10cZFRhpU2RQl7HKEbheV39unhkYWQNbVVPb8DTEZhOr';
+var APP_ID = 'layer:///apps/staging/52e7c9b4-e9cb-11e5-a188-7d4ed71366e8';
+
+var ParticipantsController = function(route) {
+  this.layerClient = new LayerAPI({
+    token: API_TOKEN,
+    appId: APP_ID
+  });
+
+  route.post((req, res) => {
+    var user = req.body;
+    var conversationId = req.params.conversationId;
+    this.layerClient.conversations
+      .getAsync(conversationId)
+      .then((layerRes)=> this.verifyParticipants(user, layerRes.body))
+      .then((status) => {
+        res.status(status);
+        res.json(user);
+      }).catch((err) => this.errorHandler(err, res));
+  });
+};
+
+ParticipantsController.prototype.verifyParticipants = function(user, conversation) {
+  var layerId = user.layerId;
+  var results = conversation.participants.filter((uid)=> uid == layerId);
+  if (results.length > 0) {
+    return Promise.resolve(200);
+  } else {
+    return this.layerClient.conversations
+          .addParticipantsAsync(conversation.id, [layerId])
+          .then(() => this.updateMetadata(conversation, user) )
+          .then(() => Promise.resolve(201));
+  }
+};
+
+ParticipantsController.prototype.updateMetadata = function(conversation, user) {
+  var appParticipants = conversation.metadata.appParticipants;
+  var metaData = {}, maxIndex, idx = 0;
+  Object.keys(appParticipants).forEach((i) => {
+    idx = i;
+    metaData[`appParticipants.${i}`] = appParticipants[i];
+  });
+  maxIndex = parseInt(idx) + 1;
+  metaData[`appParticipants.${maxIndex}`] = user;
+  return this.layerClient.conversations
+    .setMetadataPropertiesAsync(conversation.id, metaData);
+};
+
+ParticipantsController.prototype.errorHandler = function(err, res) {
+  if (err.status) res.status(err.status);
+  else res.status(500);
+  if (err.body) res.json(err.body);
+  else if(err.message) res.json({ 'message' : err.message });
+  else res.json({ 'status' : 'fail' });
+};
+
+module.exports = ParticipantsController;
