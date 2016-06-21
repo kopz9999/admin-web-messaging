5636455e33e65114f971866cfb678e67c939fea1
Kyoto Kopz <kopz9999@gmail.com>
2016-06-21 16:17:39 -0700

    backend # find or create on messages endpoint

--- server/models/Event.js
+++ server/models/Event.js
@@ -12,30 +12,49 @@ function TimeLineEvent(opts) {
   this.user = opts.user;
   this.page = opts.page;
   this.type = opts.type;
+  this.message = opts.message;
+  this.users = opts.users;
 }
 
 function EventFactory() {
 }
 
-EventFactory.prototype.buildFromLoggedEvent = function(opts) {
-  var loggedUser = userFactoryInstance.buildFromLoggedEvent(opts);
-  var loggedSite = siteFactoryInstance.buildFromLoggedEvent(opts);
-  var loggedPage = pageFactoryInstance.buildFromLoggedEvent(opts);
-  var promises = [userFactoryInstance.findOrCreate(loggedUser),
-    siteFactoryInstance.findOrCreate(loggedSite)];
-  var event = new TimeLineEvent({
-    loggedAt: Date.now(),
-    type: 'VISIT',
-  });
+EventFactory.prototype.retrieveProperties = function (event) {
+  var promises = [userFactoryInstance.findOrCreate(event.user),
+    siteFactoryInstance.findOrCreate(event.site)];
   return Promise.all(promises).then(values => {
     event.user = values[0];
     event.site = values[1];
-    loggedPage.siteId = event.site.objectId;
-    return pageFactoryInstance.findOrCreate(loggedPage).then((storedPage)=> {
+    event.page.siteId = event.site.objectId;
+    return pageFactoryInstance.findOrCreate(event.page).then((storedPage)=> {
       event.page = storedPage;
       return event;
     });
   });
+};
+
+EventFactory.prototype.buildFromAlgolia = function(opts) {
+  var event = new TimeLineEvent({
+    loggedAt: opts.logged_at,
+    type: opts.type,
+    user: userFactoryInstance.buildFromAlgolia(opts.user),
+    site: siteFactoryInstance.buildFromAlgolia(opts.site),
+    page: pageFactoryInstance.buildFromAlgolia(opts.page),
+    message: opts.message,
+    users: opts.users,
+  });
+  return this.retrieveProperties(event);
+};
+
+EventFactory.prototype.buildFromLoggedEvent = function(opts) {
+  var event = new TimeLineEvent({
+    loggedAt: Date.now(),
+    type: 'VISIT',
+    user: userFactoryInstance.buildFromLoggedEvent(opts),
+    site: siteFactoryInstance.buildFromLoggedEvent(opts),
+    page: pageFactoryInstance.buildFromLoggedEvent(opts),
+  });
+  return this.retrieveProperties(event);
 };
 
 EventFactory.prototype.serializeToAlgolia = function(event) {
@@ -45,6 +64,8 @@ EventFactory.prototype.serializeToAlgolia = function(event) {
     user: userFactoryInstance.serializeToAlgolia(event.user),
     page: pageFactoryInstance.serializeToAlgolia(event.page),
     type: event.type,
+    message: event.message,
+    users: event.users,
   };
 };
 
--- server/resources/events.js
+++ server/resources/events.js
@@ -1,9 +1,25 @@
 var algoliaManagerInstance =
   require('../utils/AlgoliaManager').algoliaManagerInstance;
+var eventFactoryInstance =
+  require('../models/Event').eventFactoryInstance;
+
+function onError(err, res) {
+  res.status(422);
+  res.send({ message: 'Unknown content'});
+}
 
 exports.create = function(req, res){
   var index = algoliaManagerInstance.getEventsIndex();
-  index.addObject(req.body, function(err, content) {
-    res.send(content);
-  });
+  var rawEvent = req.body;
+  if (rawEvent.user && rawEvent.page && rawEvent.site) {
+    rawEvent.user.id = rawEvent.user.id || rawEvent.user.layer_id; // TODO: Remove this validation
+    eventFactoryInstance.buildFromAlgolia(rawEvent).then(event => {
+      return index.addObject(eventFactoryInstance.serializeToAlgolia(event)).then((content)=> {
+        event.objectId = content.objectID;
+        res.send(event);
+      });
+    }).catch(err => onError(err, res) );
+  } else {
+    onError(null, res);
+  }
 };
