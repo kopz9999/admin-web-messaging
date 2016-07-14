3ce86b8076f2e0ba3ce6e4fe0fa145795438e518
Kyoto Kopz <kopz9999@gmail.com>
2016-07-14 16:24:39 -0700

    followers # fixed responsive issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var values = require('postcss-modules-values');
 
 var devFlagPlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
@@ -36,8 +37,11 @@ module.exports = {
         include: __dirname,
         loader: 'babel',
       },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
       { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' }
     ]
-  }
+  },
+  postcss: [
+    values
+  ]
 };
