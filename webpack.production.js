46dad8f6b591fb507a06cdead447338447c6d86e
Kyoto Kopz <kopz9999@gmail.com>
2016-07-14 16:40:50 -0700

    followers # update webpack for module values

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var values = require('postcss-modules-values');
+
 var devFlagPlugin = new webpack.DefinePlugin({
   'process.env.NODE_ENV': '"production"',
   __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
@@ -23,8 +25,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel'], exclude: /(?:node_modules)|(?:vendor)/, include: __dirname },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
       { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' }
     ]
-  }
+  },
+  postcss: [
+    values
+  ]
 };
